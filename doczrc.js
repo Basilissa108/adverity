fb9236f068f974e997d1c6f62cda0b50a27b2bb9
Basilissa108 <basilissa.albers@gmail.com>
2021-08-08 20:57:09 +0200

    Structure styleguide

--- doczrc.js
+++ doczrc.js
@@ -2,4 +2,32 @@ export default {
     title: "Adverity",
     typescript: true,
     filterComponents: (files) => files.filter(filepath => /[w-]*.(js|jsx|ts|tsx)$/.test(filepath)),
+    menu: [
+        "RFC",
+        {
+            name: "Use cases",
+            menu: [
+                "Edit datastream",
+                "Contact support",
+                "Delete user",
+                "Apply changes",
+                "Alert"
+            ]
+        },
+        {
+            name: "Components",
+            menu: [
+                "Button",
+                "ConfirmationBar",
+                "Form",
+                "Icon",
+                "Image",
+                "Input",
+                "Modal",
+                "Textarea",
+                "Toast",
+                "Typography"
+            ]
+        }
+    ]
 }
